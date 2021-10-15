c6b837e94f2b5072098a37221cce97db3bcde565
Oihana Mendez <oihanamv92@gmail.com>
2021-10-15 23:50:20 +0200

    first commit

---
+++ src/es/Studium/Cuadrados/Cuadrados.java
@@ -0,0 +1,33 @@
+package es.Studium.Cuadrados;
+
+import java.util.Scanner;
+
+public class Cuadrados
+{
+
+	public static void main(String[] args)
+	{
+        Scanner teclado = new Scanner(System.in);
+        int[] tabla1 = new int[10];
+        int numero;
+        System.out.println("Indique un número:");
+        numero = teclado.nextInt();
+        for (int i=0;i<10;i++)
+        {
+                    tabla1[i]=i+(numero+1);
+        }
+        for (int i=0;i<10;i++)
+        {
+                    tabla1[i] = tabla1[i]*tabla1[i];
+        }
+        for (int i=0;i<10;i++)
+        {
+                    System.out.println("Tabla["+i+"]="+tabla1[i]);
+        }
+        teclado.close();
+
+
+		
+	}
+
+}
